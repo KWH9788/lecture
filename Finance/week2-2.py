873b564eb2e163c9c9257ddbaa0ebaef1f0c44cf
KWH9788 <dngus9788@gmail.com>
2025-09-12 14:55:44 +0900

    Changes to be committed: 	modified:   Finance/week2-2.py

--- Finance/week2-2.py
+++ Finance/week2-2.py
@@ -5,7 +5,7 @@ data2d = [          # 2차원 리스트
     [4, 5, 6],
     [7, 8, 9]
 ]
-
+# 2차원 배열 인덱싱 중요
 print("1번째 행 :", data2d[0])    # 1번째 행
 # 행단위 접근만 가능
 
@@ -111,4 +111,129 @@ print("="*30)
 
 print(arr[1:2], arr[1])
 print(type(arr[1:2]), type(arr[1]))
+print("="*30)
+
+arr = np.arange(20).reshape(4, 5)
+print(arr[:2])
+print("="*30)
+
+result = []
+for row in arr:
+    row_01 = [row[0], row[1]]
+    print("row_01 :", row_01)
+    result.append(row_01)
+    print("result :", result)
+
+print(result)
+print("="*30)
+
+print(arr[:2, :2])
+print("="*30)
+
+print(arr[1:4, 2:5])
+print(arr[1:, 2:])
+print("="*30)
+
+a = np.array( [1, 2, 3] )
+b = np.array( [2, 3, 4] )
+print("a :", a)
+print("b :", b)
+print("a + b :", a + b)
+print("a * b :", a * b)
+print("a % b :", a % b)
+print("="*30)
+
+print("a + 3 :", a + 3)
+print("="*30)
+
+"""
+브로드 캐스팅 조건
+1. 뒤축(열)의 길이가 같다
+2. 어느 한 축의 길이가 1이다
+"""
+
+high = [92700, 92400, 92100, 94300, 92300]
+low = [90000, 91100, 91700, 92100, 90900]
+
+arr_high = np.array(high)
+arr_low = np.array(low)
+print("arr_high :", arr_high)
+print("arr_low :", arr_low)
+
+arr_diff = arr_high - arr_low
+print("arr_diff :", arr_diff)
+print("="*30)
+
+arr_high_x3 = arr_high * 3
+arr_low_x2 = arr_low * 2
+print(arr_high_x3 + arr_low_x2)
+print("="*30)
+
+data = [
+    [92700, 92400, 92100, 94300, 92300],
+    [90000, 91100, 91700, 92100, 90900]
+]
+arr = np.array(data)
+print(arr[0] * 3 + arr[1]  * 2)
+weight = np.array([3, 2]).reshape(2, 1)     # 가중치
+print((weight * arr))
+print((weight * arr).sum(axis=0))           # axis = 0 : x축 axis = 1 : y축
+print("="*30)
+
+arr = np.array( [10, 20, 30] )
+print(arr > 10)
+print("="*30)
+
+arr = np.array([10, 20, 30])
+cond = [False, True, True]
+print(arr[ cond ])
+print("="*30)
+
+arr = np.array([10, 20, 30])
+cond = arr > 10
+print(arr[ cond ])
+print("="*30)
+
+arr = np.array([10, 20, 30])
+cond0 = arr > 10
+print(f"cond0 : {cond0}")
+cond1 = arr < 30
+print(f"cond1 : {cond1}")
+print(f"cond0 & cond1 : {cond0 & cond1}")
+print(f"cond0 | cond1 : {cond0 | cond1}")
+print(arr[ cond0 & cond1 ])
+print("="*30)
+
+arr = np.array([10, 20, 30])
+arr = np.where( arr > 10, 1, 0)
+print(arr)
+print("="*30)
+
+arr = np.array([10, 20, 30])
+arr = np.where( arr > 10, arr+10, arr-10)
+print(arr)
+print("="*30)
+
+arr = np.arange(8).reshape(4, 2)
+print(arr)
+print("="*30)
+
+print(arr.sum(axis=0))
+print(arr.sum(axis=1))
+print("="*30)
+
+print(f"np.random.randint(46, size=(2, 5)) :\n{np.random.randint(46, size=(2, 5))}")
+print("="*30)
+
+x = np.linspace(0, 10, 3)
+print(x)
+print("="*30)
+
+a = np.arange(4)
+b = np.arange(4, 8)
+c = np.vstack([a, b])
+print(a)
+print(b)
+print(f"np.vstack([a, b]) :\n{np.vstack([a, b])}")
+print(f"np.hstack([a, b]) :\n{np.hstack([a, b])}")
 print("="*30)
